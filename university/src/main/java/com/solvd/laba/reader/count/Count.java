bd6c947b1ff2f55a85415c49842994ea4e3a4126
RamiroMeza <rmeza.laba@solvd.com>
2023-01-08 01:50:08 -0300

    unique words count fixed

--- university/src/main/java/com/solvd/laba/reader/count/Count.java
+++ university/src/main/java/com/solvd/laba/reader/count/Count.java
@@ -12,10 +12,19 @@ import static java.nio.charset.Charset.defaultCharset;
 
 public abstract class Count {
     public static void countUniqueWords(String readable, String result) throws IOException {
-        TreeMap<String, Integer> countImproved = new TreeMap<>();
-        Arrays.stream(StringUtils.lowerCase(FileUtils.readFileToString(new File(readable), defaultCharset())).split("\\W+")).forEach(w -> {
-            Integer integer = (countImproved.containsKey(w)) ? countImproved.remove(w) : countImproved.put(w, w.length());
+        TreeMap<String, Integer> count = new TreeMap<>();
+        TreeSet<String> banned = new TreeSet<>();
+
+        Arrays.stream(StringUtils.lowerCase(FileUtils.readFileToString(new File(readable),
+                defaultCharset())).split("\\W+")).forEach(w -> {
+            if (count.containsKey(w)) {
+                count.remove(w);
+                banned.add(w);
+            } else if (!banned.contains(w)) {
+                count.put(w, w.length());
+            }
         });
-        FileUtils.write(new File(result), countImproved.toString().replace(", ", ",\n"), defaultCharset());
+
+        FileUtils.write(new File(result), count.toString().replace(", ", ",\n"), defaultCharset());
     }
 }
